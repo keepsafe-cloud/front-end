ff974a9e1693edbd7d4ce4870c576ae4824ed3d1
MerelExellys <merel.verdegaal@exellys.com>
2022-04-27 15:49:13 +0200

    added getdata function to script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,13 @@
+
+
 var loginForm = document.getElementById("form-login");
 var registerForm = document.getElementById("form-register");
 
 var loginBtn = document.getElementById("login-btn");
 var registerBtn = document.getElementById("register-btn");
+
+var historyRoot = document.getElementById("history-root");
+
 
 if(loginBtn && registerBtn){
 loginBtn.addEventListener("click", toggleLoginForm);
@@ -33,4 +38,33 @@ function toggleRegisterForm() {
     registerBtn.innerText = "Register";
     loginBtn.style.display = "block";
   }
+}
+
+const URL = "keepsafe.mysql.database.azure.com";
+
+function getData() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if ( this.readyState === 4 && this.status === 200 ) {
+      historyRoot.innerText = xhttp.responseText;
+      showHistory(JSON.parse(xhttp.responseText));
+    }
+  }
+  xhttp.open('GET', `${URL}`, true);
+  xhttp.send();
+}
+
+function showHistory(input) {
+  console.log(inputArray);
+
+  inputArray.forEach(item => {
+    var wrap = document.createElement("div");
+    var par = document.createElement("p");
+
+    historyRoot.append(wrap);
+    wrap.append(par);
+    wrap.classList.add("his-item");
+
+    par.innerText = `${item.timestamp} Your calculation: ${item.calc} `;
+  });
 }
